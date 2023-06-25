193cb183913a4b99c0e2b5f4158f4faf06249ddd
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2023-06-25 16:57:28 -0500

    Updated

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -22,8 +22,13 @@ public class PlayerMovement : MonoBehaviour
     public float slideTimer, slideCooldown;
     Vector3 slideScale = new Vector3(1f, 0.5f, 1f);
     public Transform player;
+    public float multiplier = 1f;
     [SerializeField] Vector3 moveDirection;
-
+    public float fallingSpeedMultiplier;
+    [SerializeField] Vector3 moveForce;
+    public float slideMaxSpeed;
+    public float normMaxSpeed;
+    public float dashDistance = 5f; // Adjust the distance of the dash as needed
     // Start is called before the first frame update
     void Start()
     {
@@ -35,57 +40,85 @@ public class PlayerMovement : MonoBehaviour
     {
         grounded = Physics.Raycast(orientation.position, Vector3.down, 2f * 0.5f + 0.2f, whatIsGround);
         Move();
-        rb.AddForce(Vector3.down * 10f * Time.deltaTime);
+        Debug.Log(rb.velocity.magnitude);
+
+
+
     }
 
     void Move()
     {
 
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
         moveDirection = orientation.forward * y + orientation.right * x;
+        moveForce = moveDirection.normalized * speed * 10f * multiplier;
         if (!isSliding) {
-            rb.AddForce(moveDirection.normalized * speed * 10f);
+            rb.AddForce(moveForce, ForceMode.Force);
+
+        } else if (isSliding)
+        {
+            rb.drag = 0;
         }
 
-        if (grounded)
+        if (grounded && !isSliding)
         {
             rb.drag = groundDrag;
+            multiplier = 1f;
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Dash();
+        }
+
         if (!grounded)
         {
             rb.drag = airDrag;
+            multiplier = 0.5f;
         }
-        if (Input.GetButton("Slide"))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             Debug.Log("Slide");
+            Slide();
+            slideTimer = 0f;
+            maxSpeed = slideMaxSpeed;
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift))
+        {
+
+            isSliding = false;
+            player.localScale = new Vector3(1f, 1f, 1f);
+            maxSpeed = normMaxSpeed;
         }
         TimerControl();
         CounterMovement();
-        if(canJump && Input.GetButtonDown("Jump"))
+        if (canJump && Input.GetButtonDown("Jump"))
         {
             Jump();
+            Debug.Log("Jump");
         }
 
     }
 
     void CounterMovement()
     {
-        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !isSliding && grounded)
         {
             // Slow down the player's velocity
             rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, counterForce * Time.deltaTime);
         }
         if (rb.velocity.magnitude > maxSpeed)
         {
-            x = 0f;
-            y = 0f;
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0, 0), counterForce / 3f * Time.deltaTime);
         }
     }
 
     void Jump()
     {
-
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        jumpTimer = 0f;
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
     void Slide()
@@ -93,14 +126,15 @@ public class PlayerMovement : MonoBehaviour
         player.localScale = slideScale;
         isSliding = true;
         rb.drag = slideDrag;
-        rb.AddForce(moveDirection.normalized * slideSpeed);
+        rb.AddForce(moveForce * slideSpeed);
+
     }
 
     void TimerControl()
     {
         jumpTimer += 1f;
         slideTimer += 1f;
-        if (jumpTimer > jumpCooldown)
+        if (jumpTimer > jumpCooldown && grounded)
         {
             canJump = true;
         }
@@ -109,7 +143,7 @@ public class PlayerMovement : MonoBehaviour
             canJump = false;
         }
 
-        if(slideTimer > slideCooldown)
+        if (slideTimer > slideCooldown)
         {
             canSlide = true;
         } else
@@ -117,4 +151,16 @@ public class PlayerMovement : MonoBehaviour
             canSlide = false;
         }
     }
-}
+
+    void Dash()
+    {
+        // Calculate the dash direction based on the player's current movement direction
+        Vector3 dashDirection = moveDirection.normalized;
+
+
+    // Apply the dash force to the Rigidbody
+    rb.AddForce(dashDirection* dashDistance, ForceMode.Impulse);
+    }
+        
+ }
+  
