0efd17fc643c1f152a0a6e42ab8b67946bf60d95
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2023-07-22 12:08:29 -0500

    Ninja

---
+++ DMG-GameDevProject-main/Assets/Explosive.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour
+{
+    public float lifeTime;
+    public float power;
+    public float radius;
+    public int hitLayer;
+    public int explodeLayer;
+    public GameObject rocket;
+    public GameObject explosiveFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Delete", lifeTime);
+    }
+
+    void Delete()
+    {
+        Destroy(rocket);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Explode");
+        if(other.gameObject.layer == hitLayer)
+        {
+            Explode();
+
+        }
+    }
+
+    void Explode()
+    {
+        
+        Vector3 explosionPos = transform.position;
+        Collider[] hits = Physics.OverlapSphere(explosionPos, radius);
+        
+        foreach (Collider hit in hits)
+        {
+            Debug.Log("Hit");
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+ 
+
+            if (rb != null)
+            {
+                Vector3 locate = new Vector3(hit.gameObject.transform.position.x, 0, hit.gameObject.transform.position.z);
+                Vector3 direction = locate - explosionPos;
+                Destroy(rocket);
+                rb.AddForce(direction * power);
+                rb.AddExplosionForce(power, explosionPos, radius, 0);
+            }
+
+            Instantiate(explosiveFX, rocket.transform.position, rocket.transform.rotation);
+        }
+    }
+}
---
+++ DMG-GameDevProject-main/Assets/PlayerMovement.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player")]
+    public Rigidbody rb;
+    public Transform orientation;
+
+    [Header("Keys")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Grounded")]
+    public LayerMask ground;
+    public float playerHeight;
+    public bool grounded;
+    public float groundDrag;
+    public float maxGroundSpeed;
+
+    [Header("Air")]
+    public float airSpeedMultiplier;
+    public float maxAirSpeed;
+
+    [Header("Movement")]
+    public bool readyToJump;
+    public float jumpForce;
+    public float maxSpeed;
+    public float speed;
+    public float jumpCooldown;
+    public float horizontalInput, verticalInput;
+    Vector3 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
+    }
+    void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, ground);
+
+
+
+        MyInput();
+        SpeedControl();
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+            maxSpeed = maxGroundSpeed;
+        } else
+        {
+            rb.drag = 0;
+            maxSpeed = maxAirSpeed;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
+    } 
+
+    void SpeedControl()
+    {
+        float fallSpeed = rb.velocity.y;
+        
+        
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            Vector3 limitedVel = rb.velocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(limitedVel.x, fallSpeed, limitedVel.z);
+        }
+    }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump= false;
+            Jump();
+            Invoke("ResetJump", jumpCooldown);
+        }
+    }
+
+    void Move()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(grounded)
+        {
+            rb.AddForce(moveDirection.normalized * speed * Time.deltaTime, ForceMode.Impulse);
+        }
+        if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * speed * Time.deltaTime * airSpeedMultiplier, ForceMode.Impulse);
+        }
+    }
+}
+  
