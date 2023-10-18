68d84d152c3507430d8c3a268cfd05ece873acf0
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2023-10-18 17:52:16 -0500

    part of level one done YIPPEEEEE!!!!!!!!
    its in the summary, bum

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
 
 public class UIManager : MonoBehaviour
 {
     public Rigidbody rb;
+    public KeyCode res = KeyCode.RightShift;
     public TMP_Text speed;
     public bool spd;
+    public GameObject player;
+    public Transform respawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +21,30 @@ public class UIManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(spd)
         {
             ShowSpeed();
         }  
+        if(Input.GetKeyUp(res))
+        {
+            Restart();
+        }
     }
 
     void ShowSpeed()
     {
         speed.text = "Speed: " + rb.velocity.magnitude;
     }
+
+    void Restart()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+
+
+    }
+
 
 }
