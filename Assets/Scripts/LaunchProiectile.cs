1be2d6f2f2451dd1327031ee86026a33005322b1
TroubleshootingIsANightmare <charlie.chai.mulkerin@gmail.com>
2024-04-05 13:20:29 -0500

    Partially fixed the launch projectile code

--- Assets/Scripts/LaunchProiectile.cs
+++ Assets/Scripts/LaunchProiectile.cs
@@ -8,25 +8,21 @@ using UnityEngine.UI;
 public class LaunchProiectile : MonoBehaviour
 {
     public GameObject projectile;
-    public GameObject gun;
     public Vector3 originalPosition;
     public bool chargable, charging;
     public float chargeTimer, chargeForce, chargeMinimum;
     public GameObject chargeBullet, originalBullet;
-    public Vector3 directionOfShake;
-    public float amplitude = 5f;
     public float speed;
     public Camera playerCam;
     public Transform shootPoint;
     public bool auto;
     public float ammo, maxAmmo;
-    public bool shooting, canShoot, reloading;
+    public bool shooting, canshoot, reloading;
     public float reloadSpeed;
     public float timeBetweenShots;
     public Image[] bulletImage;
     public Sprite bulletSprite;
     public float bulletsFired;
-    public GameObject bulletUI;
     public float spread;
     public Rigidbody player;
     public float recoil;
@@ -38,11 +34,11 @@ public class LaunchProiectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
         originalPosition = transform.position;
         firing = false;
-        canShoot = true;
-        AmmoIconControl();
+        canshoot = true;
+        ammoIconControl();
         player = GameObject.Find("Player").GetComponent<Rigidbody>();
         animator.SetBool("Idle", true);
         animator.SetBool("Fire", false);
@@ -53,95 +49,66 @@ public class LaunchProiectile : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        AmmoIconControl();
+
+        ammoIconControl();
+
+        checkCanshoot();
 
 
 
 
-
-        if ((reloading || resetting || ammo <= 0))
-        {
-            canShoot = false;
-        }
-        else if (!resetting && ammo > 0 || !reloading && ammo > 0) canShoot = true; 
-        if(firing)
+        if (firing)
         {
             animator.SetBool("Fire", true);
             animator.SetBool("Idle", false);
-        } else
+        }
+        else
         {
-            animator.SetBool("Idle", true) ;
-            animator.SetBool("Fire", false) ;
+            animator.SetBool("Idle", true);
+            animator.SetBool("Fire", false);
         }
 
     }
 
-     void Update()
+    void Update()
     {
-        directionOfShake = transform.forward;
-        if (Input.GetKey(KeyCode.Mouse0) && auto)
-        {
-            shooting = true;
-        }
-        else if(!chargable)
-        {
-            shooting = Input.GetKeyDown(KeyCode.Mouse0);
-        }
-        if(Input.GetKey(KeyCode.Mouse0) && chargable && canShoot && !reloading)
-        {
-            charging = true;
-        } else
-        {
-            chargeTimer = 0f;
-            charging = false;
-        }
-        if(Input.GetKeyUp(KeyCode.Mouse0) && chargable && canShoot && !reloading)
-        {
-            shooting = true;
-        }
+        checkShooting();
 
         if (Input.GetKeyDown(KeyCode.R) && ammo < maxAmmo && !reloading)
         {
-            Reload();
+            reload();
         }
 
-        if (shooting && canShoot && ammo > 0)
-        {
-            firing = true;
-            canShoot = false;
-            Shoot();
 
-        }
-        if(charging)
+        if (charging)
         {
-            chargeTimer += Time.deltaTime*3f;
+            chargeTimer += Time.deltaTime * 3f;
 
         }
         if (chargeTimer >= chargeMinimum)
         {
             projectile = chargeBullet;
-            
+
             if (chargeTimer > 20f)
             {
                 chargeTimer = 20f;
             }
-        } else
+        }
+        else
         {
             projectile = originalBullet;
         }
         chargeForce = chargeTimer * 150f;
     }
 
-    void Shoot()
+    void shoot()
     {
         animator.SetBool("Fire", true);
-        
-        ResetShot();
 
-        canShoot = false;
-        shooting = false;
-        AmmoIconControl();
+        resetShot();
+
+
+        ammoIconControl();
 
         RaycastHit hit;
         Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -155,17 +122,23 @@ public class LaunchProiectile : MonoBehaviour
         else
         {
             targetPoint = ray.GetPoint(75);
-
-
         }
 
         Vector3 direction = targetPoint - shootPoint.position;
         player.AddForce(direction.normalized * -1 * (recoil + chargeForce));
 
 
+
+        multishot();
+
+
+        ammo--;
+    }
+
+    void multishot() {
         for (int i = 0; i < bulletsFired; i++)
         {
-            canShoot = false;
+            canshoot = false;
             GameObject currentBullet = Instantiate(projectile, shootPoint.position, Quaternion.identity);
 
             currentBullet.transform.forward = direction.normalized;
@@ -174,62 +147,86 @@ public class LaunchProiectile : MonoBehaviour
             currentBullet.GetComponent<Rigidbody>().AddForce(currentBullet.transform.forward * (speed), ForceMode.Impulse);
 
         }
-
-
-
-        
-        ammo--;
     }
 
-    void ResetShot()
+    void resetShot()
     {
         resetting = true;
-        canShoot= false;
-        Invoke("FinishReset", timeBetweenShots);
-        
+        canshoot = false;
+        Invoke("finishReset", timeBetweenShots);
     }
 
-    void FinishReset()
+    void finishReset()
     {
-        
         resetting = false;
         firing = false;
     }
 
-    void Reload()
+    void reload()
     {
         reloading = true;
-        canShoot= false;
+        canshoot = false;
         charging = false;
         Invoke("FinishReload", reloadSpeed);
         animator.SetBool("Reloading", true);
     }
 
-    void FinishReload()
+    void finishReload()
     {
         ammo = maxAmmo;
         animator.SetBool("Reloading", false);
         reloading = false;
     }
 
-    void AmmoIconControl()
+    void ammoIconControl()
     {
-        
-        for(int i = 0; i < bulletImage.Length; i++)
+
+        for (int i = 0; i < bulletImage.Length; i++)
         {
-           
-            
+
+
             bulletImage[i].sprite = bulletSprite;
             bulletImage[i].gameObject.transform.position = new Vector3(bulletImage[0].transform.position.x + distanceBetweenIcons * i, bulletImage[0].transform.position.y, bulletImage[0].transform.position.z);
-            if(i >= ammo)
+            if (i >= ammo)
             {
                 bulletImage[i].enabled = false;
-            } else
-            {
-                bulletImage[i].enabled = true;  
             }
-            
+            else
+            {
+                bulletImage[i].enabled = true;
+            }
+
         }
 
+    }
+
+    void checkCanshoot()
+    {
+        if ((reloading || resetting || ammo <= 0))
+        {
+            canshoot = false;
+        }
+        else if (ammo > 0)
+        {
+            if (!resetting || !reloading)
+            {
+                canshoot = true;
+            }
+        }
+    }
+    void checkShooting() {
+        if(auto) {
+            shooting = Input.GetKey(KeyCode.Mouse0);
+        } else {
+            if(!chargable) shooting = Input.GetKeyDown(KeyCode.Mouse0);
+            if(chargable && canshoot) charging = Input.GetKey(KeyCode.Mouse0); else chargeTimer = 0f;
+            if(charging) shooting = Input.GetKeyUp(KeyCode.Mouse0);
+        }
+        if (shooting && canshoot && ammo > 0)
+        {
+            firing = true;
+            canshoot = false;
+            shoot();
+        }
     }
 }
